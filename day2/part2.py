d1b54782846974c4bc5b20582b80baa8b913d895
William Starheim Vallestad <wstarvall@gmail.com>
2025-12-06 12:40:46 +0100

    Day 2 part 2 solved

--- day2/part2.py
+++ day2/part2.py
@@ -2,7 +2,7 @@ def splitIntoChunks(number, n):
     stringifyNumber = str(number)
     
     if len(stringifyNumber) % n != 0:
-        return None  # or raise an error
+        return None
     
     chunkSize = len(stringifyNumber) // n
     chunks = [stringifyNumber[i:i + chunkSize] for i in range(0, len(stringifyNumber), chunkSize)]
@@ -16,24 +16,21 @@ def getDivisors(n):
     return divisors
 
 def isNumberInvalid(number):
-    print(f"Number: {number}")
     stringifyNumber = str(number)
         
     divisorsOfNumber = getDivisors(len(stringifyNumber))
-    halfOfNumber = len(stringifyNumber) // 2
-    isNumberInvalid = True
-    
+    isNumberInvalid = False
+        
     for divisor in divisorsOfNumber:
-        divisorChunks = splitIntoChunks(number, divisor)
-        print(f"Chunks for divisor {divisorChunks}")
+        if(divisor != 1):
+            divisorChunks = splitIntoChunks(number, divisor)
+            areAllChunksTheSame = len(set(divisorChunks)) == 1
         
-        firstChunk = divisorChunks[0]
-        
-        if all(chunk == firstChunk for chunk in divisorChunks):
-            isNumberInvalid = True
-            break
+            if(areAllChunksTheSame):
+                isNumberInvalid = True
+                break
 
-    return (stringifyNumber[:halfOfNumber] == stringifyNumber[halfOfNumber:]) or isNumberInvalid
+    return isNumberInvalid
 
 fileReader = open("day2/input.txt", "r")
 textLine = fileReader.read()
@@ -44,6 +41,8 @@ intervals = textLine.split(",")
 sumOfInvalidNumbers = 0
 listOfInvalidNumbers = []
 
+print(f"Calculating...")
+
 for interval in intervals:
     intervalNumbers = interval.split("-")
     startOfInterval = int(intervalNumbers[0])
@@ -51,9 +50,7 @@ for interval in intervals:
 
     for number in range(startOfInterval, endOfInterval + 1):        
         if isNumberInvalid(number):
-            print(f"{number} is invalid and is added to invalid numbers")
             listOfInvalidNumbers.append(number)
             sumOfInvalidNumbers += number
                 
 print(f"Sum of invalid numbers: {sumOfInvalidNumbers}")
-print(f"List of invalid numbers {listOfInvalidNumbers}")
