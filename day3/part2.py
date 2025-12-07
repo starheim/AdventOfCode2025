959721c946301077857cf67151c7338b1d1f1712
William <wstarvall@gmail.com>
2025-12-07 11:57:45 +0100

    Further work on day 3 part 2

--- day3/part2.py
+++ day3/part2.py
@@ -1,28 +1,39 @@
 import time
 
-biggestJoltage = 0
 lengthOfBank = None
-biggestJoltage = 0
+totalJoltage = 0
+MAX_BATTERIES = 12
 
-def findLargestJoltageForBank(bank, listOfJoltages):
-    for i in range(lengthOfBank):
-        if(remainingCalls == lengthOfBank):
-            return sum
-            
+def findLargestJoltageForBank(bank, listOfJoltages, biggestJoltage):
+    print(f"Bank: {bank}")
+    print(f"List of joltages: {listOfJoltages}")
+    print(f"Length og listOfJoltages: {len(listOfJoltages)}")
+    if(len(listOfJoltages) < MAX_BATTERIES):
+        for i in range(0, len(bank)):
+            print(f"Bank[0] = {bank[0]}")
+            listOfJoltages.append(bank[0])
+            findLargestJoltageForBank(bank[i + 1:], listOfJoltages + bank[i-1], biggestJoltage) 
+    
+    else:
+        currentJoltage = sum(listOfJoltages)
+        if(currentJoltage > biggestJoltage):
+            biggestJoltage = currentJoltage
+        return
+
 
 fileReader = open("day3/input.txt", "r")
 textLine = fileReader.readlines()
 fileReader.close()
 
-totalJoltage = 0
-
 startTime = time.time()
 
 for line in textLine:
+    biggestJoltage = 0
     bank = line.strip()
     if(lengthOfBank == None):
         len(str(bank))
-    totalJoltage += findLargestJoltageForBank(bank, lengthOfBank)
+    findLargestJoltageForBank([int(ch) for ch in bank], [], biggestJoltage)
+    totalJoltage += biggestJoltage
     
 endTime = time.time()   
     
